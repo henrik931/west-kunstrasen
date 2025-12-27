122863e9004a7d154a19d0dc16de15457b1fc8fd
Henrik <h.eliasmoeller@web.de>
2025-12-27 15:35:36 +0100

    fix prisma v2

---
+++ lib/prisma.ts
@@ -0,0 +1,26 @@
+import { PrismaClient } from "@prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+import pg from "pg";
+
+const globalForPrisma = globalThis as unknown as {
+  prisma?: PrismaClient;
+  pool?: pg.Pool;
+};
+
+const pool =
+  globalForPrisma.pool ??
+  new pg.Pool({
+    connectionString: process.env.DATABASE_URL, // runtime pooled URL
+  });
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.pool = pool;
+
+const adapter = new PrismaPg(pool);
+
+export const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    adapter,
+  });
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
